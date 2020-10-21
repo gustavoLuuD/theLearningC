ae5a0222b3ff86448ebb3c1ea86529d029308d23
Gustavo Martins <gusluud@gmail.com>
2020-10-21 20:15:09 -0300

    Hello world commit
    We gave a hello to the world!

---
+++ Basics/helloWorld.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//the \n scape sequence moves the cursor to the next line
+//the \t works like a tab button
+//the \a makes a little noise
+
+int main(void) {
+  printf("\t\aHello World\n");
+  printf("here goes nothing");
+  return 0;
+}
