9ccc55a128481f9151e230c17fd6e4aaba6a74dc
Gustavo Martins <gusluud@gmail.com>
2020-10-30 11:23:46 -0300

    Update pointers.c

--- CBasics/pointers.c
+++ CBasics/pointers.c
@@ -1,6 +1,23 @@
-#import <stdio.h>
-#import <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+//with pointers we don't need to return a value
+void sum(int* a, int* b, int* sum){
+  *sum = *a  + *b;
+
+}
+int show(int a){
+  printf("%d\n",a);
+  a++;
+  printf("%d\n",a);
+}
 
 int main(void){
-  printf("This is just a test please come back later:|");
+  int a = 10;
+  int b = 2;
+  int soma;
+  //the character & takes the memory adress
+  show(a);
+  printf("%d\n",a);
+  sum(&a, &b, &soma);
+  printf("This is the sum: %i\n", soma);
 }
