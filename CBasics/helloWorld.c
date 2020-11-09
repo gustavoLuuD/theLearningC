233078cfbf4db89bc6c0948bd9f75b9135f35bbf
Gustavo Martins <gusluud@gmail.com>
2020-11-09 19:37:35 -0300

    Update helloWorld.c

--- CBasics/helloWorld.c
+++ CBasics/helloWorld.c
@@ -4,6 +4,8 @@
 //the \n scape sequence moves the cursor to the next line
 //the \t works like a tab button
 //the \a makes a little noise
+// dude \
+this is another way to  comment
 
 int main(void) {
   printf("\t\aHello World\n");
