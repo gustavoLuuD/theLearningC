f30703bc712a4972463068d21efa2eecf5ff3889
Gustavo Martins <gusluud@gmail.com>
2020-11-09 19:59:04 -0300

    Create Constants.c

---
+++ Advanced/Constants.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void){
+  15.75;
+
+  printf("This is one way to show a float-> %f\n",1.575E1);
+  printf("This is another-> %f \n",1575e-2);
+  printf("\nHere's one more -> %f",.001575e4);
+  return 0;
+}
