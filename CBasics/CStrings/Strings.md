5ee7cca7b1f0ecc29fbc3747ab07ac8a750afc0e
Gustavo Martins <gusluud@gmail.com>
2021-02-16 11:25:27 -0300

    Update strings

---
+++ CBasics/CStrings/Strings.md
@@ -0,0 +1,4 @@
+#Introdução
+
+- Strings em c são arrays de caracteres que terminam no caractere nulo '\0'.
+- Não precisamos utilizar o & ao ler uma string com *scanf()*.
---
+++ CBasics/CStrings/first.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+  char cor[50];
+  printf("Escreva uma cor ");
+  scanf( "%s",cor);
+  printf("Vc escreveu %s\n",cor);
+  return 0;
+}
